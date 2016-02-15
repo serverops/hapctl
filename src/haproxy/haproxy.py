2c62147f5c3d5807b5c42c8a2b358410482a4b0f
Chris Fidao <fideloper@gmail.com>
2016-02-15 08:06:29 -0600

    code cleanup

--- src/haproxy/haproxy.py
+++ src/haproxy/haproxy.py
@@ -1,51 +1,45 @@
 # -*- coding: utf-8 -*-
 
-import logging
-import socket
 import select
-import sys
 import socket
-import string
 
-from time import time
-from traceback import format_exc
- 
-logger = logging.getLogger(__name__)
- 
-class TimeoutException(Exception): 
-	pass 
+
+class TimeoutException(Exception):
+    pass
+
 
 class HAProxyStats(object):
-	""" Used for communicating with HAProxy through its local UNIX socket interface.
-	"""
-	def __init__(self, socket_name=None):
-		self.socket_name = socket_name
- 
-	def execute(self, command, timeout=200):
-		""" Executes a HAProxy command by sending a message to a HAProxy's local
-		UNIX socket and waiting up to 'timeout' milliseconds for the response.
-		"""
-		
-		buffer = ""
- 
-		client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-		client.connect(self.socket_name)
+    """ Used for communicating with HAProxy through its local UNIX socket interface.
+    """
+    def __init__(self, socket_name=None):
+        self.socket_name = socket_name
 
-		client.send(command + "\n")
+    def execute(self, command, timeout=200):
+        """ Executes a HAProxy command by sending a message to a HAProxy's local
+        UNIX socket and waiting up to 'timeout' milliseconds for the response.
+        :param command: command to send the socket
+        :param timeout: socket response timeout
+        """
 
-		running = True
-		while(running):
-			r, w, e = select.select([client,],[],[], timeout)
+        buffer = ""
 
-			if not (r or w or e):
-				raise TimeoutException()
-	
-			for s in r:
-				if (s is client):
-					buffer = buffer + client.recv(16384)
-					running = (len(buffer)==0)
-										
-		client.close()
+        client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        client.connect(self.socket_name)
 
-		return (buffer.decode('utf-8').split('\n'))
+        client.send(command + "\n")
 
+        running = True
+        while running:
+            r, w, e = select.select([client], [], [], timeout)
+
+            if not (r or w or e):
+                raise TimeoutException('HAProxy socket timeout (%s) reached' % timeout)
+
+            for s in r:
+                if s is client:
+                    buffer = buffer + client.recv(16384)
+                    running = (len(buffer) == 0)
+
+        client.close()
+
+        return buffer.decode('utf-8').split('\n')
